64e1385977a408d012deebede3b7668359e4d7db
Shayma Mukaram <shaymamukaram@gmail.com>
2018-09-14 08:43:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const logDriverNames = function(drivers){
-drivers.forEach(function(driver){
- console.log(driver.name);
-});
+const logDriverNames = function (drivers) {
+  drivers.forEach(function (driver) {
+    console.log(driver.name);
+  });
 };
-/* 2 */
 
-const logDriversByHometown= function(drivers,location)
-{
-  drivers.forEach(function(driver){
-    if(driver.location===location)
-    console.log(driver.name)
-   
-  })
-}
-const driversByRevenue = function(drivers)
-{
- return drivers.slice().sort(function(driver1,driver2){
-   return driver1.revenue - driver2.revenue;
- 
- });
+const logDriversByHometown = function (drivers, hometown) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
+    }
+  });
 };
-const driversByName = function(drivers)
-{
- return driver.slice().sort(function(driver1,driver2)
- {
-   return driver1.name.localeCompare(driver2.name);
- });
+
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.revenue - driverTwo.revenue;
+  });
 };
-const totallRevenue = function(drivers)
-{
- let totall=0;
- driver.forEach(function(driver){totall+=driver.revenue});};
+
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
