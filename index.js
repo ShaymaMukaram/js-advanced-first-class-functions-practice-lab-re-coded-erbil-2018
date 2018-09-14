57c5b9610b5e18ac9219c3fb430a2938e0b54de9
Shayma Mukaram <shaymamukaram@gmail.com>
2018-09-14 08:42:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const logDriverNames = function(drivers){
+drivers.forEach(function(driver){
+ console.log(driver.name);
+});
+};
+/* 2 */
+
+const logDriversByHometown= function(drivers,location)
+{
+  drivers.forEach(function(driver){
+    if(driver.location===location)
+    console.log(driver.name)
+   
+  })
+}
+const driversByRevenue = function(drivers)
+{
+ return drivers.slice().sort(function(driver1,driver2){
+   return driver1.revenue - driver2.revenue;
+ 
+ });
+};
+const driversByName = function(drivers)
+{
+ return driver.slice().sort(function(driver1,driver2)
+ {
+   return driver1.name.localeCompare(driver2.name);
+ });
+};
+const totallRevenue = function(drivers)
+{
+ let totall=0;
+ driver.forEach(function(driver){totall+=driver.revenue});};
